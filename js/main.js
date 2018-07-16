5d9643ec8469bfc92cfa4b8bcd5e5a9e5eb7c72b
Engrid Hernandez <engrid.hernandez@gmail.com>
2018-07-16 10:33:30 -0400

    project update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // what can the user do?
 // what does the user expect?
 // What can the user see?
+
+//user can upload a video
+//button w/ event listener
+//
+
+//can share video link for
+
+document.addEventListener("DOMContentLoaded",function(){
+
+  document.addEventListener("onclick", function(){
+    
+  })
+
+});
